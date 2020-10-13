cd138003a2cee4cf93fbcd2f532542dbfbe16ae6
rahulravuri <71250496+rahulravuri@users.noreply.github.com>
2020-10-13 19:30:51 +0530

    Add files via upload

---
+++ Angularjs/assignments/module1/apps.js
@@ -0,0 +1,37 @@
+(function(){
+	'use strict';
+	
+	
+	
+	var x= angular.module('Lunch',[]);
+	x.controller('fun',function($scope)
+	{
+	$scope.name ;
+	$scope.z;
+	$scope.click = function(){
+	 if(!$scope.name){console.log("no"); $scope.z ="pls enter correct data";
+	 document.getElementById("data").style.color='red';}
+     else{
+		document.getElementById("data").style.color='black';
+		var x =$scope.name;
+		console.log("1st");
+		var y =x.split(",");
+		console.log(y.length);
+		var z1=0;
+		var i;
+		console.log("2nd");
+		
+		  for (i=0 ;i< y.length;i++){
+            console.log("3rd");
+			 if(y[i]==""){console.log("no")}
+			 else{z1=z1+1;}
+
+		  }	
+		  if (z1<=3){$scope.z="enjoy"}
+		  else{$scope.z="Too much"}
+	 }
+	
+		
+	};})
+
+})();
